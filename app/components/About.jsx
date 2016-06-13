fe6ab44c9e644b571ccc93adaf4ec032284d6fd4
Erick Bernal Navarro <Erick Bernal Navarro>
2016-06-12 22:04:54 -0500

    Style about page & link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,15 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About Component</h3>
+      <h1 className="text-center">About Component</h1>
       <p>About Paragraph</p>
+      <p>
+        Tools:
+      </p>
+      <ul>
+        <li><a href="https://facebook.github.io/react">React</a>-- JavaScript Framework</li>
+        <li><a href="https://openweathermap.org">Open Weather Map</a>-- API</li>
+      </ul>
     </div>
   )
 };
